9361a0cb7d412c0677502f22ca155876d90a3a3b
Tom Tobar <cocdetombomb@gmail.com>
2022-12-17 23:27:53 -0600

    compltete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,35 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        self_map_word = self.map_word(self.word)
+        matching_words = []
+        print("This is the map word", self_map_word)
+        for word in list:
+            if Anagram.map_word(self, word) == self_map_word:
+                matching_words.append(word)
+
+        return matching_words
+                
+
+    def map_word(self, word):
+        map = {}
+        for letter in word: 
+            if letter in map.keys():
+                map[letter] += 1
+            else:
+                map[letter] = 1
+        return map
+
+
+                
+
+
+
+
+
+                
+
